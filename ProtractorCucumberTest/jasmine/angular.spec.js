cd70942500751c7b42a87f0e1698d05ef7d56c62
shiva <shiva@shivarashnasair.home>
2019-01-05 13:06:35 +0000

    test changes

--- ProtractorCucumberTest/jasmine/angular.spec.js
+++ ProtractorCucumberTest/jasmine/angular.spec.js
@@ -1,5 +1,6 @@
 //import {pageObject} from "../pages/angularPO.js"; //does not work in jasmine
 const pageObject1 = require("../pages/angularJasminePO");
+var rmeote = require("selenium-webdriver/remote");
 
 describe('angular test',function(){
 
@@ -8,5 +9,23 @@ describe('angular test',function(){
        object.pageObject.login1();
        //pageObject.login();
        pageObject1.login1();
+
+       element.all(by.css("").count().then(function(count){
+
+
+       }));
+
+       element.all(by.css("").each(function(item){
+            expect(item.getText()).to.equal("");
+
+       }));
+
+       browser.setFileDetector(new remote.FileDetector());
+
+       element(by.css("")).getText().then(function (text){
+           var temp = text;
+       });
+
+
    });
 });
